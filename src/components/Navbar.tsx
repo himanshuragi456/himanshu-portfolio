9433b5dcc6793c8affd3f18589eaf0040a9e7b55
Himanshu Ragi <hirehimanshuragi@gmail.com>
2023-06-29 11:30:48 +0530

    fixed nav items

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import logo from "../static/images/logo.png"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import "./scss/navbar.scss"
 
 const Navbar = () => {
+
+    const navigate = useNavigate()
 
     return (
         <nav className="navbar">
@@ -14,17 +16,17 @@ const Navbar = () => {
             </div>
             <div className="navbar__right">
                 <ul className="navbar__list">
-                    <li className="navbar__items">
-                        <a href="/#about" className="navbar__itemsLink"><span className="navbar__itemsLinkNumeric">01.</span>.about()</a>
+                    <li className="navbar__items" onClick={() => window.scrollTo(0, 700)}>
+                        <p className="navbar__itemsLink"><span className="navbar__itemsLinkNumeric">01.</span>.about()</p>
                     </li>
-                    <li className="navbar__items">
-                        <a href="/#about" className="navbar__itemsLink"><span className="navbar__itemsLinkNumeric">02.</span>.skills()</a>
+                    <li className="navbar__items" onClick={() => window.scrollTo(0, 970)}>
+                        <p className="navbar__itemsLink"><span className="navbar__itemsLinkNumeric">02.</span>.skills()</p>
                     </li>
-                    <li className="navbar__items">
-                        <a href="/#about" className="navbar__itemsLink"><span className="navbar__itemsLinkNumeric">03.</span>.work()</a>
+                    <li className="navbar__items" onClick={() => window.scrollTo(0, 1470)}>
+                        <p className="navbar__itemsLink"><span className="navbar__itemsLinkNumeric">03.</span>.work()</p>
                     </li>
-                    <li className="navbar__items">
-                        <a href="/#about" className="navbar__itemsLink"><span className="navbar__itemsLinkNumeric">04.</span>.contact()</a>
+                    <li className="navbar__items" onClick={() => navigate('/contact')}>
+                        <p className="navbar__itemsLink"><span className="navbar__itemsLinkNumeric">04.</span>.contact()</p>
                     </li>
                 </ul>
                 <a href="https://drive.google.com/file/d/1ZES7MGSyy-N8uZqOtV-ovhGW6p4AUVKn/view" target="_blank" rel='noreferrer' className="navbar__button">Resume</a>
